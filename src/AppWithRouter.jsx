de257204f0b43db75146a7a2dfc5372490a92d95
Kyohei Seo <kyo199333@gmail.com>
2021-01-15 19:59:05 +0900

    add component including router

---
+++ src/AppWithRouter.jsx
@@ -0,0 +1,17 @@
+// App.jsと統合したらApp.jsxにファイル名を変更する
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Top from "./pages/Top";
+import { VideosIndex } from "./pages/videos/VideosIndex";
+
+export const AppWithRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Top}></Route>
+        <Route exact path="/videos" component={VideosIndex}></Route>
+      </Switch>
+    </Router>
+  );
+};
