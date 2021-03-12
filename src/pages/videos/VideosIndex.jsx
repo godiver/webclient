9ac2d6309e7301434503e2c3feb441faa0bea8a0
Kyohei Seo <kyo199333@gmail.com>
2021-03-12 19:48:43 +0900

    fetch videos from youtube

--- src/pages/videos/VideosIndex.jsx
+++ src/pages/videos/VideosIndex.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+import { fetchVideos } from "../../api/videos";
 
 export const VideosIndex = () => {
   return (
     <div></div>
   )
 }
+  const [loading, setLoading] = useState(false);
+  const [videos, setVideos] = useState([]);
+  const query = new URLSearchParams(useLocation().search);
+
+  useEffect(() => {
+    const title = query.get("title");
+    (async () => {
+      try {
+        setLoading(true);
+        const response = await fetchVideos(title);
+        setVideos(response.data.items);
+      } catch (e) {
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
