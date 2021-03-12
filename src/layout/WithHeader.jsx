8d8392672cffb930ca0fcde8a76fd1b174ab260c
Kyohei Seo <kyo199333@gmail.com>
2021-03-12 19:47:40 +0900

    add layout

---
+++ src/layout/WithHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Header } from "../component/Header";
+
+export const WithHeader = () => {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+};
