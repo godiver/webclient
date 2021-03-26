2b23c3079642b56d876cdf5133c8e6c99654ce40
Norimitsu-Shimizu <sparesound31@gmail.com>
2021-03-26 20:28:19 +0900

    add retrieve and display the api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import './App.css';
-
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      isLoaded: false,
-      items: []
+      books: []
     }
   }
 
   componentDidMount() {
-    return url = "";
-    axios.get (url).then((res) => {
-      this.setState({ book: res.data });
+    axios.get(`/api/v1/books`).then((res) => {
+      this.setState({ books: res.data.Items })
+    }).catch(error => {
+      return error;
     });
+  }
+
+  renderBooks = () => {
+    return this.state.books.map((book, index) => (
+      <li key={index}>
+        {book.Item.author}
+      </li>
+    ))
   }
 
   render() {
     return (
       <div>
-        <p className="title"><span>book summary movie</span></p>
+        <p className="title"><span>booktube</span></p>
         <div>
           <p>
             選んだ本からその本の要約しているyoutubeの動画を視聴することができます。
@@ -33,10 +40,12 @@ class App extends Component {
             本一覧情報を取得してここに表示&詳細ページのリンクをつける
           </p>
         </div>
+        <div>
+          { this.renderBooks() }
+        </div>
       </div>
     )
   }
 }
-
 
 export default App;
