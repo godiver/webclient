691257e5e76345b558207d578eedf61aff8aecba
Nnsuke24 <nnsuke24@gmail.com>
2021-05-14 19:15:46 +0900

    update 視聴ページにチャンネル名が表示されるように変更

--- src/pages/watch/Watch.jsx
+++ src/pages/watch/Watch.jsx
@@ -27,10 +27,15 @@ export const WatchIndex = () => {
   return (
     <WithHeader>
       <div className="flex flex-col items-center">
+      <div
+          className="sm:w-full w-3/5 mb-2 cursor-pointer"
+        >
+          {video.snippet ? video.snippet.title : <p>タイトル</p>}
+        </div>
         <div
           className="sm:w-full w-3/5 mb-2 cursor-pointer"
         >
-          {video.snippet ? video.snippet.title : <p>ない</p>}
+          {video.snippet ? video.snippet.channelTitle : <p>チャンネル名</p>}
         </div>
       </div>
     </WithHeader>
