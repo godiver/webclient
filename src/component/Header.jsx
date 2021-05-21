7178410b4ed7f292f7c4e1a9e86beb7ce1c07e08
Nnsuke24 <nnsuke24@gmail.com>
2021-05-21 21:49:44 +0900

    update ヘッダータイトルの色を変更

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <div className="relative bg-main-500">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-          <div className="flex justify-start lg:w-0 lg:flex-1">BookTube</div>
+          <div className="flex justify-start lg:w-0 lg:flex-1 text-gray-50">BookTube</div>
         </div>
       </div>
     </div>
