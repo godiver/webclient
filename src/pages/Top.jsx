2103be3c9910c0fa11231b649527b1d1a8e41504
Norimitsu-Shimizu <sparesound31@gmail.com>
2021-06-14 00:47:16 +0900

    useEffectの内容を修正」する

--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -1,19 +1,17 @@
 import React, { Component, useEffect, useState } from 'react';
 import axios from 'axios'
-import { useHistory, useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 export const VideosSearch = () => {
-  const [videos, setBooks] = useState([]);
-  const query = new URLSearchParams(useLocation().search);
+  const [books, setBooks] = useState([]);
   const history = useHistory();
 
   useEffect(() => {
-    const title = query.get("title");
     (async () => {
       try {
         setLoading(true);
-        const response = await fetchVideos(title);
-        setVideos(response.data.items);
+        const response = await fetchVideosIndex();
+        setBooks(response.data.items);
       } catch (e) {
       } finally {
         setLoading(false);
@@ -22,49 +20,49 @@ export const VideosSearch = () => {
   }, []);
 
   const handleClick = (e) => {
-    const clickedVideoId = e.currentTarget.getAttribute("videoid");
-    const video = videos.find((video) => video.id.videoId === clickedVideoId);
-    history.push(`/videos?title=${video.id.videoId}`);
+    const clickedBookId = e.currentTarget.getAttribute("title");
+    const book = books.find((book) => book.Item.title === clickedBookId);
+    history.push(`/videos?title=${book.Item.title}`);
   };
-class Top extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: []
-    }
-  }
+// class Top extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       books: []
+//     }
+//   }
 
-  componentDidMount() {
-    axios.get(`/api/v1/books`).then((res) => {
-      this.setState({ books: res.data.Items })
-    }).catch(error => {
-      return error;
-    });
-  }
+//   componentDidMount() {
+//     axios.get(`/api/v1/books`).then((res) => {
+//       this.setState({ books: res.data.Items })
+//     }).catch(error => {
+//       return error;
+//     });
+//   }
 
-  renderBooks = () => {
-    return this.state.books.map((book, index) => (
-      <li key={index}>
-        <Link to={`/books/videos/?title=${book.Item.title}`}>{book.Item.title}</Link>
-      </li>
-    ))
-  }
+//   renderBooks = () => {
+//     return this.state.books.map((book, index) => (
+//       <li key={index}>
+//         <Link to={`/books/videos/?title=${book.Item.title}`}>{book.Item.title}</Link>
+//       </li>
+//     ))
+//   }
 
-  render() {
-    return (
-      <div>
-        <p className="title"><span>booktube</span></p>
-        <div>
-          <p>
-            選んだ本からその本の要約しているyoutubeの動画を視聴することができます。
-          </p>
-        </div>
-        <div>
-          { this.renderBooks() }
-        </div>
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <p className="title"><span>booktube</span></p>
+//         <div>
+//           <p>
+//             選んだ本からその本の要約しているyoutubeの動画を視聴することができます。
+//           </p>
+//         </div>
+//         <div>
+//           { this.renderBooks() }
+//         </div>
+//       </div>
+//     )
+//   }
+// }
 
-export default Top;
+// export default Top;
