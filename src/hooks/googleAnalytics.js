06ad410621f2a2aac82b535118c18268cd706083
Kyohei Seo <kyo199333@gmail.com>
2021-06-25 23:27:44 +0900

    use REACT_APP_GTAG_ID

--- src/hooks/googleAnalytics.js
+++ src/hooks/googleAnalytics.js
@@ -2,11 +2,9 @@ import { useEffect } from "react";
 
 export function useGoogleAnalytics() {
   useEffect(() => {
-    console.log(process.env.REACT_APP_GTAG_ID);
-    console.log(process.env.GTAG_ID);
     if (process.env.NODE_ENV === "production") return;
     const scriptSettingGoogleAnalytics = document.createElement("script");
-    scriptSettingGoogleAnalytics.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG_ID}`;
+    scriptSettingGoogleAnalytics.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GTAG_ID}`;
     scriptSettingGoogleAnalytics.async = true;
     document.body.appendChild(scriptSettingGoogleAnalytics);
     const scriptProcessingGoogleAnalytics = document.createElement("script");
@@ -14,6 +12,6 @@ export function useGoogleAnalytics() {
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', ${process.env.GTAG_ID});`;
+    gtag('config', ${process.env.REACT_APP_GTAG_ID});`;
   }, []);
 }
