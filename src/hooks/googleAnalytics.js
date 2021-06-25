8fd373d174276b8cbb4b4a336a91d2422a0a82d7
Kyohei Seo <kyo199333@gmail.com>
2021-06-26 00:05:04 +0900

    append in head tag

--- src/hooks/googleAnalytics.js
+++ src/hooks/googleAnalytics.js
@@ -6,13 +6,13 @@ export function useGoogleAnalytics() {
     const scriptSettingGoogleAnalytics = document.createElement("script");
     scriptSettingGoogleAnalytics.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GTAG_ID}`;
     scriptSettingGoogleAnalytics.async = true;
-    document.body.appendChild(scriptSettingGoogleAnalytics);
+    document.head.appendChild(scriptSettingGoogleAnalytics);
     const scriptProcessingGoogleAnalytics = document.createElement("script");
     scriptProcessingGoogleAnalytics.text = `
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', ${process.env.REACT_APP_GTAG_ID});`;
-    document.body.appendChild(scriptProcessingGoogleAnalytics);
+    document.head.appendChild(scriptProcessingGoogleAnalytics);
   }, []);
 }
