ddde7080c6bf993c04f47883250de677d2bccf41
Kyohei Seo <kyo199333@gmail.com>
2021-06-26 00:55:05 +0900

    modify

--- src/hooks/googleAnalytics.js
+++ src/hooks/googleAnalytics.js
@@ -1,25 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 
 export function useGoogleAnalytics() {
-  // const [completed, setCompleted] = useState(false);
+  const [completed, setCompleted] = useState(false);
   const { listen } = useHistory();
-  // useEffect(() => {
-  //   if (process.env.NODE_ENV === "production") return;
-  //   const scriptSettingGoogleAnalytics = document.createElement("script");
-  //   scriptSettingGoogleAnalytics.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GTAG_ID}`;
-  //   scriptSettingGoogleAnalytics.async = true;
-  //   document.head.appendChild(scriptSettingGoogleAnalytics);
-  //   const scriptProcessingGoogleAnalytics = document.createElement("script");
-  //   scriptProcessingGoogleAnalytics.text = `
-  //   window.dataLayer = window.dataLayer || [];
-  //   function gtag(){dataLayer.push(arguments);}
-  //   gtag('js', new Date());
-  //   gtag('config', ${process.env.REACT_APP_GTAG_ID});`;
-  //   document.head.appendChild(scriptProcessingGoogleAnalytics);
+  useEffect(() => {
+    if (process.env.NODE_ENV !== "production") return;
+    const scriptSettingGoogleAnalytics = document.createElement("script");
+    scriptSettingGoogleAnalytics.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GTAG_ID}`;
+    scriptSettingGoogleAnalytics.async = true;
+    document.head.appendChild(scriptSettingGoogleAnalytics);
+    const scriptProcessingGoogleAnalytics = document.createElement("script");
+    scriptProcessingGoogleAnalytics.text = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', ${process.env.REACT_APP_GTAG_ID});`;
+    document.head.appendChild(scriptProcessingGoogleAnalytics);
 
-  //   setCompleted(true);
-  // }, []);
+    setCompleted(true);
+  }, []);
 
   useEffect(() => {
     listen((location) => {
@@ -28,5 +28,5 @@ export function useGoogleAnalytics() {
         page_path: location.pathname,
       });
     });
-  }, [listen]);
+  }, [listen, completed]);
 }
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -3,22 +3,6 @@ import React from "react";
 import { useGoogleAnalytics } from "../hooks/googleAnalytics";
 
 export const Test = () => {
-  const setGA = () => {
-    if (process.env.NODE_ENV !== "production") return;
-    const scriptSettingGoogleAnalytics = document.createElement("script");
-    scriptSettingGoogleAnalytics.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GTAG_ID}`;
-    scriptSettingGoogleAnalytics.async = true;
-    document.head.appendChild(scriptSettingGoogleAnalytics);
-    const scriptProcessingGoogleAnalytics = document.createElement("script");
-    scriptProcessingGoogleAnalytics.text = `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', ${process.env.REACT_APP_GTAG_ID});`;
-    document.head.appendChild(scriptProcessingGoogleAnalytics);
-  };
-  setGA();
-
   useGoogleAnalytics();
 
   return (
