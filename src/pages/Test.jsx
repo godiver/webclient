9a957dcc9ad5c8fe8d07428fc1ab7cc86a6c1e96
Kyohei Seo <kyo199333@gmail.com>
2021-06-26 00:38:25 +0900

    modify

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -4,6 +4,18 @@ import { useGoogleAnalytics } from "../hooks/googleAnalytics";
 
 export const Test = () => {
   useGoogleAnalytics();
+  if (process.env.NODE_ENV === "production") return;
+  const scriptSettingGoogleAnalytics = document.createElement("script");
+  scriptSettingGoogleAnalytics.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GTAG_ID}`;
+  scriptSettingGoogleAnalytics.async = true;
+  document.head.appendChild(scriptSettingGoogleAnalytics);
+  const scriptProcessingGoogleAnalytics = document.createElement("script");
+  scriptProcessingGoogleAnalytics.text = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', ${process.env.REACT_APP_GTAG_ID});`;
+  document.head.appendChild(scriptProcessingGoogleAnalytics);
 
   return (
     <div>
