7b828f1a8438a87bed2f599533d9c7e29bd6e55d
Norimitsu-Shimizu <sparesound31@gmail.com>
2021-08-20 19:06:49 +0900

    環境変数を設定

--- src/api/videos.js
+++ src/api/videos.js
@@ -1,13 +1,15 @@
 import { api } from "./index";
 
+const apiURL = process.env.REACT_APP_BACKEND_API_BASE_URL
+
 export const fetchVideosIndex = async () => {
-  return api.get(`/api/v1/books`);
+  return api.get(`${apiURL}/api/v1/books`);
 };
 
 export const fetchVideos = async (title) => {
-  return api.get(`/api/v1/books/videos/${title}`);
+  return api.get(`${apiURL}/api/v1/books/videos/${title}`);
 };
 
 export const fetchVideo = async (videoId) => {
-  return api.get(`/api/v1/watch/${videoId}`);
+  return api.get(`${apiURL}/api/v1/watch/${videoId}`);
 };
