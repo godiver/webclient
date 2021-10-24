5e434a49f5652782847e0f08b3bc3d8b2bd0fe47
Norimitsu-Shimizu <sparesound31@gmail.com>
2021-10-24 16:18:22 +0900

    feat: 検索した本を表示するためにBookListSearchedコンポーネントを作成した

---
+++ src/pages/top/BookListSearched.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const BookListSearched = ({responseSearchBooks, handleClick}) => {
+  return (
+    <>
+      {responseSearchBooks.map((book) => (
+        <div
+          key={book.Item.title}
+          title={book.Item.title}
+          onClick={handleClick}
+          className="cursor-pointer"
+        >
+          <div className="py-2 px-2">
+            <img src={book.Item.largeImageUrl} alt="Logo" className="w-60 h-auto" />
+          </div>
+        </div>
+      ))}
+    </>
+  )
+}
