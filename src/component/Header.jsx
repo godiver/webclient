8d5386314b5adfd033b1926317c7aed97865c6df
Nnsuke24 <nnsuke24@gmail.com>
2021-11-05 19:55:11 +0900

    不要なボーダー削除

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <div className="relative bg-main-500">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
-        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+        <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1 text-gray-50">
             <Link to={`/`}>BookTube</Link>
           </div>
