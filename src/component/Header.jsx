2e6c886a9179aec78492bf4dc065e9d58e5d52b4
Nnsuke24 <nnsuke24@gmail.com>
2021-11-05 19:34:47 +0900

    アイコンクリックでトップへ遷移

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from 'react-router-dom';//　追加　Linkタブを読み込む
 
 export const Header = () => {
   return (
     <div className="relative bg-main-500">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-          <div className="flex justify-start lg:w-0 lg:flex-1 text-gray-50">BookTube</div>
+          <div className="flex justify-start lg:w-0 lg:flex-1 text-gray-50">
+            <Link to={`/`}>BookTube</Link>
+          </div>
         </div>
       </div>
     </div>
