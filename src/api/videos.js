18c28121568f0eb8451886007a86fe57a0450140
Nnsuke24 <nnsuke24@gmail.com>
2022-02-25 18:31:41 +0900

    Update envファイル

--- src/api/videos.js
+++ src/api/videos.js
@@ -1,6 +1,6 @@
 import { api } from "./index";
 
-const apiURL = process.env.REACT_APP_BACKEND_API_BASE_URL
+const apiURL = process.env.REACT_APP_API_BASE_URL
 
 export const fetchVideosIndex = async () => {
   return api.get(`${apiURL}/api/v1/books`);
