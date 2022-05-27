d1135d1370bb048d0e7ce481cd037a2f655f5cf1
Norimitsu-Shimizu <sparesound31@gmail.com>
2022-05-27 19:26:43 +0900

    feat: ルートティングにsignupを追加

--- src/AppWithRouter.jsx
+++ src/AppWithRouter.jsx
@@ -8,6 +8,7 @@ import { VideosSearch } from "./pages/top/Top";
 import { VideosIndex } from "./pages/videos/VideosIndex";
 import { WatchIndex } from "./pages/watch/Watch";
 import { Test } from "./pages/Test";
+import { SignUp } from "./pages/signup/SignUp";
 
 export const AppWithRouter = () => {
   return (
@@ -17,6 +18,7 @@ export const AppWithRouter = () => {
         <Route exact path="/videos" component={VideosIndex}></Route>
         <Route exact path="/watch" component={WatchIndex}></Route>
         <Route exact path="/test" component={Test}></Route>
+        <Route exact path="/signup" component={SignUp}></Route>
       </Switch>
     </Router>
   );
