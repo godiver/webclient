7a048ab8267c92a2f1cc53def89d788d2a53c035
Norimitsu-Shimizu <sparesound31@gmail.com>
2022-06-03 19:23:58 +0900

    feat: メールアドレスとパスワードの入力欄の作成とアドレスとパスワードのstateを設定

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const SignUp = () => {
+
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+
   return(
     <>
-      <div>
-        Hello
-      </div>
+      <h1>アカウント作成</h1>
+      <form>
+        <div>
+          <label>メールアドレス</label>
+          <input name="email" type="email" value={registerEmail} onChange={(e) => setRegisterEmail(e.target.value)} placeholder="email" />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input name="password" type="password" value={registerPassword} onChange={(e) => setRegisterPassword(e.target.value)} />
+        </div>
+        <div>
+          <button>登録</button>
+        </div>
+      </form>
     </>
   )
 };
