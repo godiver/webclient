fea30c8fef620e837e5cfbb3c906d75242396aa8
Norimitsu-Shimizu <sparesound31@gmail.com>
2022-06-17 19:06:32 +0900

    feat: ログインページのビューファイルを作成

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+
+export const Login = () => {
+
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+    } catch(error) {
+      alert("正しく入力してください");
+    }
+  }
+
+  return(
+    <>
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>メールアドレス</label>
+          <input
+            name="email"
+            type="email"
+            value={registerEmail}
+            onChange={(e) => setRegisterEmail(e.target.value)}
+            placeholder="email"
+          />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input
+            name="password"
+            type="password"
+            value={registerPassword}
+            onChange={(e) => setRegisterPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button>ログイン</button>
+        </div>
+      </form>
+
+      
+    </>
+  )
+};
